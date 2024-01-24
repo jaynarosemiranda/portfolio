8d2ec9f1aaaa16bde12ca920bc0ee6f9738ab3a5
Jayna Rose Miranda <work.jaynarosemiranda@gmail.com>
2024-01-24 19:18:47 +0800

    updates

---
+++ app.js
@@ -0,0 +1,33 @@
+function goToMenu() {
+  let navlinks = document.querySelector("#nav-links");
+  //   navlinks.classList.add("hidden");
+
+  console.log("hello!");
+}
+function myFunction(e) {
+  var elems = document.querySelector(".active");
+  if (elems !== null) {
+    elems.classList.remove("active");
+  }
+  e.target.classList.add("active");
+  // if (e.target.innerHTML == "Contact") {
+  //   e.target.style.color = "white";
+  // }
+}
+
+function toggleSystem() {
+  // let body = document.querySelector("body");
+  let body = document.querySelector("body");
+  let links = document.querySelectorAll("a");
+
+  if (body.classList.contains("light-theme")) {
+    console.log("true");
+    document.querySelector(".fa-moon").style.display = "none";
+    document.querySelector(".fa-sun").style.display = "block";
+    body.classList.remove("light-theme");
+  } else {
+    document.querySelector(".fa-moon").style.display = "block";
+    document.querySelector(".fa-sun").style.display = "none";
+    body.classList.add("light-theme");
+  }
+}
